5309ce5dc97a32a6c7c395f9b327473545f14ba3
Unknown <claudiu@megheaiulian.com>
2017-10-23 16:56:31 +0300

    Test stanza messages

--- src/incoming.js
+++ src/incoming.js
@@ -1,6 +1,43 @@
-module.exports = (bp, userlike) => {
+
+const isFromBot = (stanza)=>{
   
+}
+
+const preprocessStanza = (stanza)=> {
+  
+}
+
+
+
+module.exports = (bp, userlike) => {
+
   userlike.client.on('stanza', function(stanza){
-    console.log('⮈', stanza.toString())
+    if(isFromBot(stanza)) return;
+
+    if(stanza.is('message')){
+      bp.middlewares.sendIncoming({
+        type: stanza.type,
+        platform: 'userlike',
+        // text: stanza.getChildText('body'),
+        text: 'text',
+        raw: stanza
+        // user: user 
+      })
+    }
+
+    // if (stanza.is('presence')){
+    //   console.log('🗸 stanza is presence')
+    // }
+    // if (stanza.is('message') && stanza.getChildText('body') !== null){
+    //   console.log('⮞ stanza text: ' + stanza.getChildText('body'));
+    // }
+
+    // console.log('⮈', stanza.toString());
+  
+    console.log(' TO   : ' + stanza.attrs.to);
+    console.log(' FROM : ' + stanza.attrs.from);
+    if (stanza.is('message') && stanza.getChildText('body') !== null){
+      console.log('⮞ stanza text: ' + stanza.getChildText('body'));
+    }
   });
 }
